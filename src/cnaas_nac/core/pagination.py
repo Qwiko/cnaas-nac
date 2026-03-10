c4ab3498bb4b8fdd20b4e488353ea372e97eedb7
Jakob Mellberg <jakob.mellberg@gmail.com>
2026-03-10 09:24:31 +0000

    Allow 0 perPage pagination

--- src/cnaas_nac/core/pagination.py
+++ src/cnaas_nac/core/pagination.py
@@ -6,7 +6,7 @@ class PaginationParams:
     def __init__(
         self,
         page: int = Query(1, ge=1, description="Page number"),
-        size: int = Query(10, ge=1, le=100, description="Items per page"),
+        size: int = Query(10, ge=0, le=1000, description="Items per page"),
     ):
         self.page = page
         self.size = size
