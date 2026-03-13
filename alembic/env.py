017ee4e97f406fce25b41f012bf50a415fc7b517
Jakob Mellberg <jakob.mellberg@gmail.com>
2026-03-13 13:46:11 +0000

    Added nas table

--- alembic/env.py
+++ alembic/env.py
@@ -24,10 +24,11 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from cnaas_nac.models.policy import *  # noqa: E402, F403
-from cnaas_nac.models.nas_port import *  # noqa: E402, F403
-from cnaas_nac.models.radacct import *  # noqa: E402, F403
 from cnaas_nac.models.endpoint import *  # noqa: E402, F403
+from cnaas_nac.models.nas import *  # noqa: E402, F403
+from cnaas_nac.models.nas_port import *  # noqa: E402, F403
+from cnaas_nac.models.policy import *  # noqa: E402, F403
+from cnaas_nac.models.radacct import *  # noqa: E402, F403
 from cnaas_nac.models.radpostauth import *  # noqa: E402, F403
 from cnaas_nac.models.rbac import *  # noqa: E402, F403
 
---
+++ src/cnaas_nac/models/nas.py
@@ -0,0 +1,48 @@
+import datetime
+from typing import Any, Optional
+
+from sqlalchemy import (
+    DateTime,
+    Integer,
+    PrimaryKeyConstraint,
+    Text,
+    text,
+)
+from sqlalchemy.dialects.postgresql import INET
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import CIDR
+from pydantic.networks import IPvAnyNetwork
+from .base import Base, TimestampsMixin
+
+
+class Nas(Base, TimestampsMixin):
+    __tablename__ = "nas"
+    __table_args__ = (PrimaryKeyConstraint("id", name="nas_pkey"),)
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
+    description: Mapped[Optional[str]] = mapped_column(Text)
+
+    network: Mapped[IPvAnyNetwork] = mapped_column(CIDR, index=True, unique=True)
+
+    type: Mapped[str] = mapped_column(
+        Text, nullable=False, server_default=text("'other'::text")
+    )
+    secret: Mapped[str] = mapped_column(Text, nullable=False)
+    require_ma: Mapped[str] = mapped_column(
+        Text, nullable=False, server_default=text("'auto'::text")
+    )
+    limit_proxy_state: Mapped[str] = mapped_column(
+        Text, nullable=False, server_default=text("'auto'::text")
+    )
+    # ports: Mapped[Optional[int]] = mapped_column(Integer)
+
+
+class NasReload(Base):
+    __tablename__ = "nasreload"
+    __table_args__ = (PrimaryKeyConstraint("nasipaddress", name="nasreload_pkey"),)
+
+    nasipaddress: Mapped[Any] = mapped_column(INET, primary_key=True)
+    reloadtime: Mapped[datetime.datetime] = mapped_column(
+        DateTime(True), nullable=False
+    )
