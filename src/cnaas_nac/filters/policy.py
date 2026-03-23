c163b0020bae4feb1359e7e146399b1f5525ba3c
Jakob Mellberg <jakob.mellberg@gmail.com>
2026-03-23 08:53:25 +0000

    Added more filters to policy

--- src/cnaas_nac/filters/policy.py
+++ src/cnaas_nac/filters/policy.py
@@ -2,7 +2,13 @@ from typing import Optional
 
 from fastapi_filter.contrib.sqlalchemy import Filter
 
-from cnaas_nac.models.policy import Policy
+from cnaas_nac.models.policy import (
+    ClientType,
+    MatchLogic,
+    Policy,
+    PortLocking,
+    PortType,
+)
 
 
 class PolicyFilter(Filter):
@@ -11,6 +17,30 @@ class PolicyFilter(Filter):
     name__ilike: Optional[str] = None
     name__like: Optional[str] = None
     name__neq: Optional[str] = None
+
+    client_type: Optional[ClientType] = None
+    client_type__in: Optional[list[ClientType]] = None
+    client_type__ilike: Optional[str] = None
+    client_type__like: Optional[str] = None
+    client_type__neq: Optional[ClientType] = None
+
+    match_logic: Optional[MatchLogic] = None
+    match_logic__in: Optional[list[MatchLogic]] = None
+    match_logic__ilike: Optional[str] = None
+    match_logic__like: Optional[str] = None
+    match_logic__neq: Optional[MatchLogic] = None
+
+    port_locking: Optional[PortLocking] = None
+    port_locking__in: Optional[list[PortLocking]] = None
+    port_locking__ilike: Optional[str] = None
+    port_locking__like: Optional[str] = None
+    port_locking__neq: Optional[PortLocking] = None
+
+    port_type: Optional[PortType] = None
+    port_type__in: Optional[list[PortType]] = None
+    port_type__ilike: Optional[str] = None
+    port_type__like: Optional[str] = None
+    port_type__neq: Optional[PortType] = None
 
     order_by: list[str] = ["name"]
 
