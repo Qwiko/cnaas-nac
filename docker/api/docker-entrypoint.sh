f8e462fad4024d2875a6eda108622001d76f780b
Jakob Mellberg <jakob.mellberg@gmail.com>
2026-04-16 06:20:49 +0000

    Update api start script

--- docker/api/docker-entrypoint.sh
+++ docker/api/docker-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
+# TODO changed allowed forwarded-allow-ips to "*" for now, but should be changed to specific IPs in production
 if [ "$1" = 'external' ]; then
-    set -- fastapi run src/cnaas_nac/api_external/main.py --port 8000
+    set -- fastapi run src/cnaas_nac/api_external/main.py --port 8000 --forwarded-allow-ips="*"
 elif [ "$1" = 'internal' ]; then
-    set -- fastapi run src/cnaas_nac/api_internal/main.py --port 8000
+    set -- fastapi run src/cnaas_nac/api_internal/main.py --port 8000 --forwarded-allow-ips="*"
 else
     echo "Mode: $1 is not supported." >& 2
     exit 1
