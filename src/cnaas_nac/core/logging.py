25ead84cedd49d2f1f16d5f65c0b0db358e3108e
Jakob Mellberg <jakob.mellberg@gmail.com>
2026-05-07 06:14:51 +0000

    Remove fix for logging handler, created duplicate logs

--- src/cnaas_nac/core/logging.py
+++ src/cnaas_nac/core/logging.py
@@ -6,11 +6,5 @@ from cnaas_nac.core.settings import settings
 def get_logger() -> logging.Logger:
     logger = logging.getLogger("cnaas-nac")
 
-    if not logger.handlers:
-        formatter = logging.Formatter("%(levelname)s in %(module)s: %(message)s")
-        handler = logging.StreamHandler()
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-
     logger.setLevel(settings.LOGGING)
     return logger
